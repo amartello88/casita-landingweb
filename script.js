39414b65f68c26466fedc3b37446bc837bacaa4a
amartello <lunafreelancerr@gmail.com>
2025-11-02 17:42:02 -0300

    popup-js-img banner

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+/* pop up img galeria section */
+    document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.image-grid img');
+
+    // Crear el lightbox y la X
+    const lightbox = document.createElement('div');
+    lightbox.classList.add('lightbox');
+    
+    const img = document.createElement('img');
+    const closeBtn = document.createElement('span');
+    closeBtn.classList.add('lightbox-close');
+    closeBtn.innerHTML = '&times;'; // la X
+    
+    lightbox.appendChild(img);
+    lightbox.appendChild(closeBtn);
+    document.body.appendChild(lightbox);
+
+    // Mostrar imagen clickeada
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+        img.src = image.src;
+        lightbox.classList.add('active');
+        });
+    });
+
+    // Cerrar al hacer clic en la X
+    closeBtn.addEventListener('click', () => {
+        lightbox.classList.remove('active');
+    });
+
+    // Cerrar al hacer clic fuera de la imagen
+    lightbox.addEventListener('click', (e) => {
+        if (e.target === lightbox) {
+        lightbox.classList.remove('active');
+        }
+    });
+    });
